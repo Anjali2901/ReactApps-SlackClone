ad7356c7ce4a8e09a26afe4a566d5580909e5ffc
Anjali Kumari <lit2016033@iiita.ac.in>
2020-10-25 00:53:23 +0530

    signin page first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import  {App,SignIn} from './componentA';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <SignIn /> ,
+   document.getElementById('root')
 );
 
