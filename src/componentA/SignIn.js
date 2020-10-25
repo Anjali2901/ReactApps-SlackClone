6df2fd36e79b2c47b38f1a46693f5c0d59e4a5f7
Anjali Kumari <lit2016033@iiita.ac.in>
2020-10-25 19:02:36 +0530

    signin signup form

--- src/componentA/SignIn.js
+++ src/componentA/SignIn.js
@@ -4,7 +4,12 @@ import React , { Component } from 'react';
 class SignIn extends Component {
     render() {
         return (
-            <div> SignIn </div>
+            <div className="signin-form">
+                <h1>Sign In Sign Up</h1>
+                <form>
+                    <button>Sign in with Google/ Sign up with Google</button>
+                </form>                 
+            </div>
         );
     }
 }
